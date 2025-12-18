496926fb752424fb6a5a08469e66ad3d83ab9e3e
Sai-Govardhan <24331a05i6@mvgrce.edu.in>
2025-12-18 23:36:49 +0530

    Add files via upload

---
+++ precedence.py
@@ -0,0 +1,9 @@
+#24331A05I6
+
+print("expression 1 : 5+3*2 = ",5+3*2)
+
+print("expression 2 : 2*3**2 = ",2*3**2)
+
+print("expression 2 : (2**3)**2 = ",(2**3)**2)
+
+print("expression 3 : 2**3**2 = ",2**3**2)
